9293074955d42ca699765c06498aaeb84f84bb41
VictorBlancoTellez <victorblancotellez@gmail.com>
2020-12-16 11:44:46 +0100

    cambio total 2

--- Trafico/src/socket/Cliente.java
+++ Trafico/src/socket/Cliente.java
@@ -1,36 +1,34 @@
 package socket;
-
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
+import java.io.*;
+import java.net.*;
+ 
 public class Cliente {
-
-	private static String HOST = "localhost";
-	private static int PUERTO = 2017;
-
-	public void startClient() throws IOException {
-		Socket socket;
-		DataOutputStream mensaje;
-
-		try {
-			// Creamos nuestro socket
-			socket = new Socket(HOST, PUERTO);
-
-			mensaje = new DataOutputStream(socket.getOutputStream());
-
-			// Enviamos un mensaje
-			mensaje.writeUTF("Hola soy un cliente!!");
-
-			// Cerramos la conexión
-			socket.close();
-
-		} catch (UnknownHostException e) {
-			System.out.println("El host no existe o no está activo.");
-		} catch (IOException e) {
-			System.out.println("Error de entrada/salida.");
-		}
-
-	}
+ 
+    private static String HOST = "localhost";
+    private static int PUERTO = 2017;
+ 
+    public static void main(String args[]) {
+         
+        Socket socket;
+        DataOutputStream mensaje;
+         
+        try {
+            //Creamos nuestro socket
+            socket = new Socket(HOST, PUERTO);
+     
+            mensaje = new DataOutputStream(socket.getOutputStream());
+ 
+            //Enviamos un mensaje
+            mensaje.writeUTF("Hola soy un cliente!!");
+ 
+            //Cerramos la conexión
+            socket.close();
+ 
+        } catch (UnknownHostException e) {
+            System.out.println("El host no existe o no está activo.");
+        } catch (IOException e) {
+            System.out.println("Error de entrada/salida.");
+        }
+         
+    }
 }
--- Trafico/src/socket/Servidor.java
+++ Trafico/src/socket/Servidor.java
@@ -1,64 +1,55 @@
 package socket;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
-public class Servidor extends Conexion {
-	private static int PUERTO = 1234;
-	public Servidor() throws IOException {
-		super("servidor");
-	}
+public class Servidor {
+	private static int PUERTO = 2017;
 
-	public void startServer() throws IOException {
-		while (true) {
-			 BufferedReader entrada;
-		        DataOutputStream salida;
-		        Socket socket;
-		        ServerSocket serverSocket;
-		         
-		        try {
-		            serverSocket = new ServerSocket(PUERTO);
-		 
-		            System.out.println("Esperando una conexión...");
-		 
-		            socket = serverSocket.accept();
-		 
-		            System.out.println("Un cliente se ha conectado...");
-		 
-		            // Para los canales de entrada y salida de datos
-		 
-		            entrada = new BufferedReader(new InputStreamReader(
-		                    socket.getInputStream()));
-		 
-		            salida = new DataOutputStream(socket.getOutputStream());
-		 
-		            System.out.println("Confirmando conexion al cliente....");
-		 
-		            salida.writeUTF("Conexión exitosa...");
-		 
-		            // Para recibir el mensaje
-		 
-		            String mensajeRecibido = entrada.readLine();
-		 
-		            System.out.println(mensajeRecibido);
-		 
-		            salida.writeUTF("Se recibio tu mensaje.");
-		 
-		            salida.writeUTF("Gracias por conectarte.");
-		 
-		            System.out.println("Cerrando conexión...");
-		 
-		            // Cerrando la conexón
-		            serverSocket.close();
-		 
-		        } catch (IOException e) {
-		            System.out.println("Error de entrada/salida."  + e.getMessage());
-		        }
-		 
-		    }
+	public static void main(String args[]) {
+
+		BufferedReader entrada;
+		DataOutputStream salida;
+		Socket socket;
+		ServerSocket serverSocket;
+
+		try {
+			serverSocket = new ServerSocket(PUERTO);
+
+			System.out.println("Esperando una conexión...");
+
+			socket = serverSocket.accept();
+
+			System.out.println("Un cliente se ha conectado...");
+
+			// Para los canales de entrada y salida de datos
+
+			entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			salida = new DataOutputStream(socket.getOutputStream());
+
+			System.out.println("Confirmando conexion al cliente....");
+
+			salida.writeUTF("Conexión exitosa...");
+
+			// Para recibir el mensaje
+
+			String mensajeRecibido = entrada.readLine();
+
+			System.out.println(mensajeRecibido);
+
+			salida.writeUTF("Se recibio tu mensaje.");
+
+			salida.writeUTF("Gracias por conectarte.");
+
+			System.out.println("Cerrando conexión...");
+
+			// Cerrando la conexón
+			serverSocket.close();
+
+		} catch (IOException e) {
+			System.out.println("Error de entrada/salida." + e.getMessage());
+		}
+
 	}
 }
